34827d8d9c15045d34f5316744c75d35a4ca968f
unlomtrois <vadim.kuz02@gmail.com>
2022-12-13 18:28:03 +0300

    kek

--- src/utils.py
+++ src/utils.py
@@ -3,14 +3,11 @@
 def soderzhit_tochku(stroka: str) -> bool:
     return stroka.count(".") > 0
 
-def ne_soderzhit_tochku(stroka: str) -> bool:
-    return not soderzhit_tochku(stroka)
-
 def is_number(stroka: str) -> bool:
     """проверяет строку value на то, что оно может быть приведено к типу float"""
     if stroka.isdigit():
         return True
-    if ne_soderzhit_tochku(stroka):
+    if not soderzhit_tochku(stroka):
         return False
     first_part, second_part = stroka.split(".")
     if first_part.isdigit() and second_part.isdigit():

85f7086fd8ac4bb7a008c4d6f3f76ebae7965222
unlomtrois <vadim.kuz02@gmail.com>
2022-12-13 20:29:09 +0300

    fix

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 """Дополнительные функции"""
 
-def is_number(stroka: str) -> bool:
+def is_number(string: str) -> bool:
     """проверяет строку value на то, что оно может быть приведено к типу float"""
-    if stroka.isdigit():
+    if string.isdigit():
         return True
-    if stroka.count(".") == 0:
+    if string.count(".") == 0:
         return False
-    first_part, second_part = stroka.split(".")
+    first_part, second_part = string.split(".")
     if first_part.isdigit() and second_part.isdigit():
         return True
 

e65d14c601f5498c66c2b90d5a849df2fc5f28f4
unlomtrois <vadim.kuz02@gmail.com>
2022-12-13 18:30:15 +0300

    kekek

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,10 @@
 """Дополнительные функции"""
 
-def soderzhit_tochku(stroka: str) -> bool:
-    return stroka.count(".") > 0
-
 def is_number(stroka: str) -> bool:
     """проверяет строку value на то, что оно может быть приведено к типу float"""
     if stroka.isdigit():
         return True
-    if not soderzhit_tochku(stroka):
+    if stroka.count(".") == 0:
         return False
     first_part, second_part = stroka.split(".")
     if first_part.isdigit() and second_part.isdigit():
